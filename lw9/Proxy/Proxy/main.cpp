fcffe0879528c327c0063458f7e0eadc75f3a222
LAPTOP-U0R8E398\prost <prosto_channal@mail.ru>
2024-12-03 19:29:01 +0300

    Lw9 Инициализация лабы

---
+++ lw9/Proxy/Proxy/main.cpp
@@ -0,0 +1,14 @@
+﻿#include "../lib/CoW/CoW.h"
+#include <iostream>
+
+int main()
+{
+	CoW<std::string> s1("Hello");
+	CoW<std::string> s2(s1);
+
+	s1.Write()->append("!!!");
+
+	auto write = s1.Write();
+
+	std::cout << *s1 << " " << *s2 << std::endl;
+}
